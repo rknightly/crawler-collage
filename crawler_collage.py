325aaad2f4e53be1bd38204a91332016063ae35e
rknightly <reknightly@gmail.com>
2016-05-31 20:23:51 -0500

    Initial commit

---
+++ crawler_collage.py
@@ -0,0 +1,29 @@
+# Receive url input from user
+
+class Crawler:
+    """Visit the web pages and collect the necessary information"""
+# Visit page
+
+    # Collect images
+
+        # Store in folder
+
+    # Collect links
+
+        # Store in list
+
+# Visit the stored links until level num reached. Or
+
+    # page num reached, or level num reached
+
+# Create new image with collected images
+
+    # Make the background with the first image
+
+    # Place other photos
+
+        # Add at random angle and size
+
+        # Save result
+
+# Display saved result
